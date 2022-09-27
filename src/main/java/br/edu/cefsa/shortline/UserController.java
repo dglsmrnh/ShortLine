84cfd18ddb7508a2211239b59b7db1080fcf29e7
ssimiao <sssimiao10@gmail.com>
2022-09-27 00:56:37 -0300

    user auth

---
+++ src/main/java/br/edu/cefsa/shortline/UserController.java
@@ -0,0 +1,26 @@
+package br.edu.cefsa.shortline;
+
+import br.edu.cefsa.shortline.config.security.UserDetailsServiceImpl;
+import br.edu.cefsa.shortline.persistence.entity.UserEntity;
+import br.edu.cefsa.shortline.request.UserDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private UserDetailsServiceImpl userDetailsService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveUser(@RequestBody UserDto userDto){
+        userDetailsService.saveUser(userDto);
+        URI uri = URI.create("/user");
+        return ResponseEntity.created(uri).build();
+    }
+}
---
+++ src/main/java/br/edu/cefsa/shortline/config/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package br.edu.cefsa.shortline.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    final UserDetailsServiceImpl userDetailsService;
+
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
